d8f3529b879666c9dfe065887ce7b7e74b348e21
pedroesperanca <buesimples@gmail.com>
2016-08-18 19:12:34 +0100

    code clean up, working on assets and allocations

--- config/db.js
+++ config/db.js
@@ -23,61 +23,13 @@ pool
 pool
   .query('CREATE TABLE IF NOT EXISTS assets (id SERIAL PRIMARY KEY, type VARCHAR(40), attributes jsonb)');
 
+// pool
+//   .query('DROP TABLE IF EXISTS allocations');
+
 pool
   .query('CREATE TABLE IF NOT EXISTS allocations (id SERIAL PRIMARY KEY, user_id INTEGER, asset_id INTEGER, untill TIMESTAMP)');
 
 
 module.exports = function() {
-    return {
-        movieList : [],
-        /*
-         * Save the movie inside the "db".
-         */
-        save(movie) {
-            movie.id = crypto.randomBytes(20).toString('hex'); // fast enough for our purpose
-            this.movieList.push(movie);
-            return 1;           
-        },
-        /*
-         * Retrieve a movie with a given id or return all the movies if the id is undefined.
-         */
-        find(id) {
-            if(id) {
-                return this.movieList.find(element => {
-                        return element.id === id;
-                    }); 
-            }else {
-                return this.movieList;
-            }
-        },
-        /*
-         * Delete a movie with the given id.
-         */
-        remove(id) {
-            var found = 0;
-            this.movieList = this.movieList.filter(element => {
-                    if(element.id === id) {
-                        found = 1;
-                    }else {
-                        return element.id !== id;
-                    }
-                });
-            return found;           
-        },
-        /*
-         * Update a movie with the given id
-         */
-        update(id, movie) {
-            var movieIndex = this.movieList.findIndex(element => {
-                return element.id === id;
-            });
-            if(movieIndex !== -1) {
-                this.movieList[movieIndex].title = movie.title;
-                this.movieList[movieIndex].year = movie.year;
-                return 1;
-            }else {
-                return 0;
-            }
-        }       
-    }
+    return true;
 };  
